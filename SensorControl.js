105c883d711a6ef16afbe292256a8aaa7abe7989
Matthias Kuehnle <kuehnle@online.de>
2016-01-28 01:14:20 +0100

    removed mongodb, automaic id assign  and firmwareupdate for now

--- SensorControl.js
+++ SensorControl.js
@@ -148,100 +148,6 @@ function pushDWord(arr, val) {
 	arr.push((val  >> 24) & 0x000000FF);
 }
 
-function loadFirmware(fwtype, fwversion, sketch, db) {
-	var filename = path.basename(sketch);
-        console.log("compiling firmware: " + filename);
-        var req = {
-                files: [{
-                        filename: filename,
-                        content: fs.readFileSync(sketch).toString()
-                }],
-                format: "hex",
-                version: "105",
-                build: {
-                        mcu: "atmega328p",
-                        f_cpu: "16000000L",
-                        core: "arduino",
-                        variant: "standard"
-                }
-        };
-        requestify.post('https://codebender.cc/utilities/compile/', req).then(function(res) {
-                var body = JSON.parse(res.getBody());
-                if (body.success) {
-			console.log("loading firmware: " + filename);
-			fwdata = [];
-			var start = 0;
-			var end = 0;
-			var pos = 0;
-			var hex = body.output.split("\n");
-			for(l in hex) {
-				line = hex[l].trim();
-				if (line.length > 0) {
-					while (line.substring(0, 1) != ":")
-						line = line.substring(1);
-					var reclen = parseInt(line.substring(1, 3), 16);
-					var offset = parseInt(line.substring(3, 7), 16);
-					var rectype = parseInt(line.substring(7, 9), 16);
-					var data = line.substring(9, 9 + 2 * reclen);
-					var chksum = parseInt(line.substring(9 + (2 * reclen), 9 + (2 * reclen) + 2), 16);
-					if (rectype == 0) {
-						if ((start == 0) && (end == 0)) {
-							if (offset % 128 > 0)
-								throw new Error("error loading hex file - offset can't be devided by 128");
-							start = offset;
-							end = offset;
-						}
-						if (offset < end)
-							throw new Error("error loading hex file - offset lower than end");
-						while (offset > end) {
-							fwdata.push(255);
-							pos++;
-							end++;
-						}
-						for (var i = 0; i < reclen; i++) {
-							fwdata.push(parseInt(data.substring(i * 2, (i * 2) + 2), 16));
-							pos++;
-						}
-						end += reclen;
-					}
-				}
-			}	
-			var pad = end % 128; // ATMega328 has 64 words per page / 128 bytes per page
-			for (var i = 0; i < 128 - pad; i++) {
-				fwdata.push(255);
-				pos++;
-				end++;
-			}
-			var blocks = (end - start) / FIRMWARE_BLOCK_SIZE;
-			var crc = 0xFFFF;
-			for (var i = 0; i < blocks * FIRMWARE_BLOCK_SIZE; ++i) {
-				var v = crc;
-				crc = crcUpdate(crc, fwdata[i]);
-			}
-			db.collection('firmware', function(err, c) {
-				c.update({
-					'type': fwtype,
-					'version': fwversion
-				}, {
-					$set: {
-						'filename': filename,
-						'blocks': blocks,
-						'crc': crc,
-						'data': fwdata
-					}
-				}, {
-					upsert: true
-				}, function(err, result) {
-					if (err)
-						console.log('Error writing firmware to database');
-				});
-			});
-			console.log("loading firmware done. blocks: " + blocks + " / crc: " + crc);
-                } else {
-                        console.log("error: %j", res.body);
-                }
-        });
-}
 
 /*
 function decode(msg) {
@@ -274,37 +180,7 @@ function encode(destination, sensor, command, acknowledge, type, payload) {
 	return msg.toString();
 }
 
-function saveProtocol(sender, payload, db) {
-	db.collection('node', function(err, c) {
-		c.update({
-			'id': sender
-		}, {
-			$set: {
-				'protocol': payload
-			}
-		}, {
-			upsert: true
-		}, function(err, result) {
-			if (err)
-				console.log("Error writing protocol to database");
-		});
-	});
-}
 
-function saveSensor(sender, sensor, type, db) {
-	db.collection('node', function(err, c) {
-		c.update({
-			'id': sender
-		}, {
-			$addToSet: {
-				sensor: type
-			}
-		}, function(err, result) {
-			if (err)
-				console.log("Error writing sensor to database");
-		});
-	});
-}
 var lastTmp = 0;
 var lastHum = 0;
 function sendValueToDisplay(sender, sensor, type, payload, gw) { 
@@ -315,18 +191,15 @@ function sendValueToDisplay(sender, sensor, type, payload, gw) {
    var ts = new Date().toLocaleTimeString("de-de", {hour: "2-digit", minute: "2-digit"});
    sendMessage(50, 50, gw, "OUTSIDE "+payload+" "+ts);
 }
-function saveValue(sender, sensor, type, payload, db) {
+function saveValue(sender, sensor, type, payload) {
 	var cn = "Value-" + sender.toString() + "-" + sensor.toString();
-	db.createCollection(cn, function(err, c) { 
-		c.save({
+/*
+		{
 			'timestamp': new Date().getTime(),
 			'type': type,
 			'value': payload
-		}, function(err, result) {
-			if (err)
-				console.log("Error writing value to database");
-		});
-	});
+		}
+*/
         var gauge = "UNKNOWN";
         if (sender === 103 || sender === 101 || sender == 104) {
 		  gauge = "TEMP";
@@ -399,47 +272,26 @@ function saveValue(sender, sensor, type, payload, db) {
         sendToLibrato(gauge, source, payload);
 }
 
-function saveBatteryLevel(sender, payload, db) {
+function saveBatteryLevel(sender, payload) {
+/*
 	var cn = "BatteryLevel-" + sender.toString();
-	db.createCollection(cn, function(err, c) { 
-		c.save({
-			'timestamp': new Date().getTime(),
-			'value': payload
-		}, function(err, result) {
-			if (err)
-				console.log("Error writing battery level to database");
-		});
-	});
+		{ 'timestamp': new Date().getTime(),
+	          'value': payload }
+*/
 }
 
-function saveSketchName(sender, payload, db) {
-	db.collection('node', function(err, c) {
-		c.update({
-			'id': sender
-		}, {
-			$set: {
-				'sketchName': payload
-			}
-		}, function(err, result) {
-			if (err)
-				console.log("Error writing sketch name to database");
-		});
-	});
+function saveSketchName(sender, payload) {
+/* 
+		c.update({ 'id': sender }, 
+                { $set: { 'sketchName': payload } }
+*/
 }
 
-function saveSketchVersion(sender, payload, db) {
-	db.collection('node', function(err, c) {
-		c.update({
-			'id': sender
-		}, {
-			$set: {
-				'sketchVersion': payload
-			}
-		}, function(err, result) {
-			if (err)
-				console.log("Error writing sketch version to database");
-		});
-	});
+function saveSketchVersion(sender, payload) {
+/*
+		c.update({ 'id': sender }, {
+	        $set: { 'sketchVersion': payload }
+*/
 }
 
 function sendTime(destination, sensor, gw) {
@@ -450,43 +302,6 @@ function sendTime(destination, sensor, gw) {
 	var td = encode(destination, sensor, command, acknowledge, type, payload);
 	console.log('-> ' + td.toString());
 	gw.write(td);
-}
-
-function sendNextAvailableSensorId(db, gw) {
-	db.collection('node', function(err, c) {
-		c.find({
-			$query: { },
-			$orderby: {
-				'id': 1
-			}
-		}).toArray(function(err, results) {
-			if (err)
-				console.log('Error finding nodes');
-			var id = 1;
-			for (var i = 0; i < results.length; i++)
-				if (results[i].id > i + 1) {
-					id = i + 1;
-					break;
-				}
-			if (id < 255) {
-				c.save({
-					'id': id
-				}, function(err, result) {
-					if (err)
-						console.log('Error writing node to database');
-					var destination = BROADCAST_ADDRESS;
-					var sensor = NODE_SENSOR_ID;
-					var command = C_INTERNAL;
-					var acknowledge = 0; // no ack
-					var type = I_ID_RESPONSE;
-					var payload = id;
-					var td = encode(destination, sensor, command, acknowledge, type, payload);
-					console.log('-> ' + td.toString());
-					gw.write(td);
-				});
-			}
-		});
-	});
 }
 
 function sendConfig(destination, gw) {
@@ -500,145 +315,23 @@ function sendConfig(destination, gw) {
 	gw.write(td);
 }
 
-function sendFirmwareConfigResponse(destination, fwtype, fwversion, db, gw) {
-	// keep track of type/versin info for each node
-	// at the same time update the last modified date
-	// could be used to remove nodes not seen for a long time etc.
-	db.collection('node', function(err, c) {
-		c.update({
-			'id': destination
-		}, {
-			$set: {
-				'type': fwtype,
-				'version': fwversion,
-				'reboot': 0
-			}
-		}, {
-			upsert: true
-		}, function(err, result) {
-			if (err)
-				console.log("Error writing node type and version to database");
-		});
-	});
-	if (fwtype == 0xFFFF) {
-		// sensor does not know which type / blank EEPROM
-		// take predefined type (ideally selected in UI prior to connection of new sensor)
-		if (fwDefaultType == 0xFFFF)
-			throw new Error('No default sensor type defined');
-		fwtype = fwDefaultType;
-	}
-	db.collection('firmware', function(err, c) {
-		c.findOne({
-			$query: {
-				'type': fwtype
-			},
-			$orderby: {
-				'version': -1
-			}
-		}, function(err, result) {
-			if (err)
-				console.log('Error finding firmware for type ' + fwtype);
-			else if (!result)
-				console.log('No firmware found for type ' + fwtype);
-			else {
-				var payload = [];
-				pushWord(payload, result.type);
-				pushWord(payload, result.version);
-				pushWord(payload, result.blocks);
-				pushWord(payload, result.crc);
-				var sensor = NODE_SENSOR_ID;
-				var command = C_STREAM;
-				var acknowledge = 0; // no ack
-				var type = ST_FIRMWARE_CONFIG_RESPONSE;
-				var td = encode(destination, sensor, command, acknowledge, type, payload);
-				console.log('-> ' + td.toString());
-				gw.write(td);
-			}
-		});
-	});
-}
 
-function sendFirmwareResponse(destination, fwtype, fwversion, fwblock, db, gw) {
-	db.collection('firmware', function(err, c) {
-		c.findOne({
-			'type': fwtype,
-			'version': fwversion
-		}, function(err, result) {
-			if (err)
-				console.log('Error finding firmware version ' + fwversion + ' for type ' + fwtype);
-			var payload = [];
-			pushWord(payload, result.type);
-			pushWord(payload, result.version);
-			pushWord(payload, fwblock);
-			for (var i = 0; i < FIRMWARE_BLOCK_SIZE; i++)
-				payload.push(result.data[fwblock * FIRMWARE_BLOCK_SIZE + i]);
-			var sensor = NODE_SENSOR_ID;
-			var command = C_STREAM;
-			var acknowledge = 0; // no ack
-			var type = ST_FIRMWARE_RESPONSE;
-			var td = encode(destination, sensor, command, acknowledge, type, payload);
-			console.log('-> ' + td.toString());
-			gw.write(td);
-		});
-	});
-}
-
-function saveRebootRequest(destination, db) {
-	db.collection('node', function(err, c) {
-		c.update({
-			'id': destination
-		}, {
-			$set: {
-				'reboot': 1
-			}
-		}, function(err, result) {
-			if (err)
-				console.log("Error writing reboot request to database");
-		});
-	});
-}
-
-function checkRebootRequest(destination, db, gw) {
-	db.collection('node', function(err, c) {
-		c.find({
-			'id': destination
-		}, function(err, item) {
-			if (err)
-				console.log('Error checking reboot request');
-			else if (item.reboot == 1)
-				sendRebootMessage(destination, gw);
-		});
-	});
-}
-
-function sendRebootMessage(destination, gw) {
-	var sensor = NODE_SENSOR_ID;
-        var command = C_INTERNAL;
-        var acknowledge = 0; // no ack
-        var type = I_REBOOT;
-        var payload = "";
-        var td = encode(destination, sensor, command, acknowledge, type, payload);
-        console.log('-> ' + td.toString());
-        gw.write(td);
-}
-
-
-function appendData(str, db, gw) {
+function appendData(str, gw) {
     pos=0;
     while (str.charAt(pos) != '\n' && pos < str.length) {
         appendedString=appendedString+str.charAt(pos);
         pos++;
     }
     if (str.charAt(pos) == '\n') {
-        rfReceived(appendedString.trim(), db, gw);
+        rfReceived(appendedString.trim(), gw);
         appendedString="";
     }
     if (pos < str.length) {
-        appendData(str.substr(pos+1,str.length-pos-1), db, gw);
+        appendData(str.substr(pos+1,str.length-pos-1), gw);
     }
 }
 
-function rfReceived(data, db, gw) {
+function rfReceived(data, gw) {
 	if ((data != null) && (data != "")) {
 		console.log('<-(in) ' + data);
 		// decoding message
@@ -663,12 +356,13 @@ function rfReceived(data, db, gw) {
 		// decision on appropriate response
 		switch (command) {
 		case C_PRESENTATION:
-			if (sensor == NODE_SENSOR_ID)
-				saveProtocol(sender, payload, db);
-			saveSensor(sender, sensor, type, db);
+			if (sensor == NODE_SENSOR_ID) {
+			//	saveProtocol(sender, payload, d_b);
+                        }
+			//saveSensor(sender, sensor, type, d_b);
 			break;
 		case C_SET:
-			saveValue(sender, sensor, type, payload, db);
+			saveValue(sender, sensor, type, payload);
 			sendValueToDisplay(sender, sensor, type, payload, gw);
 			break;
 		case C_REQ:
@@ -676,7 +370,7 @@ function rfReceived(data, db, gw) {
 		case C_INTERNAL:
 			switch (type) {
 			case I_BATTERY_LEVEL:
-				saveBatteryLevel(sender, payload, db);
+				saveBatteryLevel(sender, payload);
 				break;
 			case I_TIME:
 				sendTime(sender, sensor, gw);
@@ -684,7 +378,7 @@ function rfReceived(data, db, gw) {
 			case I_VERSION:
 				break;
 			case I_ID_REQUEST:
-				sendNextAvailableSensorId(db, gw);
+				// sendNextAvailableSensorId(d_b, gw);
 				break;
 			case I_ID_RESPONSE:
 				break;
@@ -702,56 +396,22 @@ function rfReceived(data, db, gw) {
 			case I_CHILDREN:
 				break;
 			case I_SKETCH_NAME:
-				saveSketchName(sender, payload, db);
+				// saveSketchName(sender, payload, d_b);
 				break;
 			case I_SKETCH_VERSION:
-				saveSketchVersion(sender, payload, db);
+				// saveSketchVersion(sender, payload, d_b);
 				break;
 			case I_REBOOT:
 				break;
 			}
 			break;
 		case C_STREAM:
-			switch (type) {
-					case ST_FIRMWARE_CONFIG_REQUEST:
-							var fwtype = pullWord(payload, 0);
-							var fwversion = pullWord(payload, 2);
-							sendFirmwareConfigResponse(sender, fwtype, fwversion, db, gw);
-							break;
-					case ST_FIRMWARE_CONFIG_RESPONSE:
-							break;
-					case ST_FIRMWARE_REQUEST:
-							var fwtype = pullWord(payload, 0);
-							var fwversion = pullWord(payload, 2);
-							var fwblock = pullWord(payload, 4);
-							sendFirmwareResponse(sender, fwtype, fwversion, fwblock, db, gw);
-							break;
-					case ST_FIRMWARE_RESPONSE:
-							break;
-					case ST_SOUND:
-							break;
-					case ST_IMAGE:
-							break;
-			}
 			break;
 		}
-		checkRebootRequest(sender, db, gw);
+		// checkRebootRequest(sender, d_b, gw);
 	}
 }
 
-var dbc = require('mongodb').MongoClient;
-dbc.connect('mongodb://' + dbAddress + ':' + dbPort + '/' + dbName, function(err, db) {
-	if(err) {
-		console.log('Error connecting to database at mongodb://' + dbAddress + ':' + dbPort + '/' + dbName);
-		return;
-	}
-	console.log('Connected to database at mongodb://' + dbAddress + ':' + dbPort + '/' + dbName);
-	db.createCollection('node', function(err, collection) { });
-	db.createCollection('firmware', function(err, collection) { });
-
-	// ToDo : check for new hex files / only load if new / get type and version from filename
-	for (var i = 0; i < fwSketches.length; i++)
-		loadFirmware(i, 1, fwSketches[i], db);
 
 	var gw;
 	if (gwType == 'Ethernet') {
@@ -761,7 +421,7 @@ dbc.connect('mongodb://' + dbAddress + ':' + dbPort + '/' + dbName, function(err
 		gw.on('connect', function() {
 			console.log('connected to ethernet gateway at ' + gwAddress + ":" + gwPort);
 		}).on('data', function(rd) {
-			appendData(rd.toString(), db, gw);
+			appendData(rd.toString(), gw);
 		}).on('end', function() {
 			console.log('disconnected from gateway');
 		}).on('error', function() {
@@ -776,7 +436,7 @@ dbc.connect('mongodb://' + dbAddress + ':' + dbPort + '/' + dbName, function(err
 		gw.on('open', function() {
 			console.log('connected to serial gateway at ' + gwPort);
 		}).on('data', function(rd) {
-			appendData(rd.toString(), db, gw);
+			appendData(rd.toString(), gw);
 		}).on('end', function() {
 			console.log('disconnected from gateway');
 		}).on('error', function() {
@@ -789,7 +449,6 @@ dbc.connect('mongodb://' + dbAddress + ':' + dbPort + '/' + dbName, function(err
 	setInterval(function() {
 		sendTime(BROADCAST_ADDRESS, NODE_SENSOR_ID, gw);
 	}, 5*60*1000);
-});
 var cnt = 0;
 function sendMessage(destination, sensor, gw, payload) {
         var command = C_SET;
